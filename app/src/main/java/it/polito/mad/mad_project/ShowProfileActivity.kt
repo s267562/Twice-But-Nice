83df02081e198ba6cb6902dcd76de22cf7d620e2
Simone <simone.sinagra96@gmail.com>
2020-03-27 15:42:32 +0100

    initial commit

---
+++ app/src/main/java/it/polito/mad/mad_project/ShowProfileActivity.kt
@@ -0,0 +1,12 @@
+package it.polito.mad.mad_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_profile)
+    }
+}
