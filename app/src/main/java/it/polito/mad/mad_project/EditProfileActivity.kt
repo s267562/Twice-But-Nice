454e2afcc0fe68bc761c78fbe0a49dea1255e1cd
ivanaorefice <s266782@studenti.polito.it>
2020-03-27 20:13:21 +0100

    EditProfile

--- app/src/main/java/it/polito/mad/mad_project/EditProfileActivity.kt
+++ app/src/main/java/it/polito/mad/mad_project/EditProfileActivity.kt
@@ -1,12 +1,30 @@
 package it.polito.mad.mad_project
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.widget.ImageButton
+import android.widget.PopupMenu
+import androidx.appcompat.app.AppCompatActivity
 
 class EditProfileActivity : AppCompatActivity() {
+
+    val cameraicon: ImageButton = findViewById(R.id.camera_icon)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile)
+        cameraicon.setOnClickListener {
+
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.option_menu, menu)
+        return true
+    }
+
+
+
 }
