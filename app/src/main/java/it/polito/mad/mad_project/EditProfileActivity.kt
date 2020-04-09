62e5d786f86d2fdb9dbb4f101db2a5e0fa9ef86d
Victor Sinapi <s265925@studenti.polito.it>
2020-04-09 17:49:49 +0200

    Fixed rotation and clean code

--- app/src/main/java/it/polito/mad/mad_project/EditProfileActivity.kt
+++ app/src/main/java/it/polito/mad/mad_project/EditProfileActivity.kt
@@ -38,8 +38,6 @@ import java.util.*
 
 class EditProfileActivity : AppCompatActivity() {
 
-    private var currentPath: String? = null
-    private var photo: Bitmap?= null
     private val CAPTURE_IMAGE_REQUEST = 1
     val SELECT_IMAGE = 2
     private var imageFile: File? = null
@@ -65,7 +63,6 @@ class EditProfileActivity : AppCompatActivity() {
         Log.d ("MAD_LOG", "RECEIVED-USER: $user")
 
         if (savedInstanceState != null) {
-            currentPath = savedInstanceState.getString("PathFile")
             imagePath =savedInstanceState.getString("ImagePath")
         }
         var image: Bitmap?=null
@@ -92,6 +89,9 @@ class EditProfileActivity : AppCompatActivity() {
         }else{
             rotation_button.visibility=View.VISIBLE
             rotation_button.setOnClickListener{
+                user_photo.setDrawingCacheEnabled(true)
+                image = user_photo.getDrawingCache(true).copy(Bitmap.Config.ARGB_8888, false)
+                user_photo.destroyDrawingCache()
                 var rotateBitmap = rotateImage(image!!, 90)
                 image = rotateBitmap
                 user_photo.setImageBitmap(image)
@@ -167,12 +167,9 @@ class EditProfileActivity : AppCompatActivity() {
         // Open Camera
         if (requestCode == CAPTURE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK){
             try {
-                val file = File(this.currentPath)
+                val file = File(this.imagePath)
                 val uri:Uri = Uri.fromFile(file)
-                var rotatedBitmap: Bitmap?
-                rotatedBitmap = handleSamplingAndRotationBitmap(applicationContext, selectedImage = uri)
-                user_photo.setImageBitmap(rotatedBitmap)
-                this.photo = rotatedBitmap
+                user_photo.setImageURI(uri)
             }catch (e: IOException){
                 e.printStackTrace()
             }
@@ -183,14 +180,11 @@ class EditProfileActivity : AppCompatActivity() {
                 val uriPic = data?.data
                 user_photo.setImageURI(uriPic)
                 if (uriPic != null) {
-                    //imageFile = File(uriPic.getPath())
-                    //imagePath = uriPic.path
                     val file: File = createImageFile()
                     val fOut: FileOutputStream = FileOutputStream(file)
                     imageFile = file
                     imagePath = file.absolutePath
                     var mBitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), uriPic)
-                    this.photo = mBitmap
                     mBitmap.compress(Bitmap.CompressFormat.JPEG,85,fOut)
                 }
 
@@ -201,114 +195,12 @@ class EditProfileActivity : AppCompatActivity() {
             displayMessage(baseContext, "Select image wrong")
         }
     }
-    /**
-     * This method is responsible for solving the rotation issue if exist. Also scale the images to
-     * 1024x1024 resolution
-     *
-     * @param context The current context
-     * @param selectedImage The Image URI
-     * @return Bitmap image results
-     * @throws IOException
-     */
-    @Throws(IOException::class)
-    fun handleSamplingAndRotationBitmap(context:Context, selectedImage:Uri): Bitmap? {
-        val MAX_HEIGHT = 1024
-        val MAX_WIDTH = 1024
-        // First decode with inJustDecodeBounds=true to check dimensions
-        val options = BitmapFactory.Options()
-        options.inJustDecodeBounds = true
-        var imageStream = context.getContentResolver().openInputStream(selectedImage)
-        BitmapFactory.decodeStream(imageStream, null, options)
-        if (imageStream != null) {
-            imageStream.close()
-        }
-        // Calculate inSampleSize
-        options.inSampleSize = calculateInSampleSize(options, MAX_WIDTH, MAX_HEIGHT)
-        // Decode bitmap with inSampleSize set
-        options.inJustDecodeBounds = false
-        imageStream = context.getContentResolver().openInputStream(selectedImage)
-        var img = BitmapFactory.decodeStream(imageStream, null, options)
-        img = img?.let { rotateImageIfRequired(context, it, selectedImage) }
-        return img
-    }
-
-    /**
-     * Calculate an inSampleSize for use in a {@link BitmapFactory.Options} object when decoding
-     * bitmaps using the decode* methods from {@link BitmapFactory}. This implementation calculates
-     * the closest inSampleSize that will result in the final decoded bitmap having a width and
-     * height equal to or larger than the requested width and height. This implementation does not
-     * ensure a power of 2 is returned for inSampleSize which can be faster when decoding but
-     * results in a larger bitmap which isn't as useful for caching purposes.
-     *
-     * @param options An options object with out* params already populated (run through a decode*
-     * method with inJustDecodeBounds==true
-     * @param reqWidth The requested width of the resulting bitmap
-     * @param reqHeight The requested height of the resulting bitmap
-     * @return The value to be used for inSampleSize
-     */
-    private fun calculateInSampleSize(options:BitmapFactory.Options,
-                                      reqWidth:Int, reqHeight:Int):Int {
-        // Raw height and width of image
-        val height = options.outHeight
-        val width = options.outWidth
-        var inSampleSize = 1
-        if (height > reqHeight || width > reqWidth)
-        {
-            // Calculate ratios of height and width to requested height and width
-            val heightRatio = Math.round(height.toFloat() / reqHeight.toFloat())
-            val widthRatio = Math.round(width.toFloat() / reqWidth.toFloat())
-            // Choose the smallest ratio as inSampleSize value, this will guarantee a final image
-            // with both dimensions larger than or equal to the requested height and width.
-            inSampleSize = if (heightRatio < widthRatio) heightRatio else widthRatio
-            // This offers some additional logic in case the image has a strange
-            // aspect ratio. For example, a panorama may have a much larger
-            // width than height. In these cases the total pixels might still
-            // end up being too large to fit comfortably in memory, so we should
-            // be more aggressive with sample down the image (=larger inSampleSize).
-            val totalPixels = (width * height).toFloat()
-            // Anything more than 2x the requested pixels we'll sample down further
-            val totalReqPixelsCap = (reqWidth * reqHeight * 2).toFloat()
-            while (totalPixels / (inSampleSize * inSampleSize) > totalReqPixelsCap)
-            {
-                inSampleSize++
-            }
-        }
-        return inSampleSize
-    }
-
-    /**
-     * Rotate an image if required.
-     *
-     * @param img The image bitmap
-     * @param selectedImage Image URI
-     * @return The resulted Bitmap after manipulation
-     */
-    @Throws(IOException::class)
-    private fun rotateImageIfRequired(context:Context, img:Bitmap, selectedImage:Uri): Bitmap? {
-        val input = context.getContentResolver().openInputStream(selectedImage)
-        val ei:ExifInterface
-        if (Build.VERSION.SDK_INT > 23)
-            ei = ExifInterface(input)
-        else
-            ei = ExifInterface(selectedImage.getPath())
-        val orientation = ei.getAttributeInt(ExifInterface.TAG_ORIENTATION, ExifInterface.ORIENTATION_NORMAL)
-        when (orientation) {
-            ExifInterface.ORIENTATION_ROTATE_90 -> return rotateImage(img, 90)
-            ExifInterface.ORIENTATION_ROTATE_180 -> return rotateImage(img, 180)
-            ExifInterface.ORIENTATION_ROTATE_270 -> return rotateImage(img, 270)
-            else -> return img
-        }
-    }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        /*if(this.photo != null){
-            outState.putParcelable("Photo", this.photo)
-        }*/
         if (this.imagePath != null) {
             outState.putString("ImagePath", this.imagePath)
         }
-        outState.putString("PathFile",this.currentPath)
     }
 
     override fun onBackPressed() {
@@ -365,17 +257,10 @@ class EditProfileActivity : AppCompatActivity() {
             ".jpg", /* suffix */
             storageDir      /* directory */
         )
-        currentPath = image.absolutePath
+        this.imagePath= image.absolutePath
         return image
     }
-    /*
-    private fun rotateImage(img:Bitmap, degree:Int):Bitmap {
-        val matrix = Matrix()
-        matrix.postRotate(degree.toFloat())
-        val rotatedImg = Bitmap.createBitmap(img, 0, 0, img.getWidth(), img.getHeight(), matrix, true)
-        img.recycle()
-        return rotatedImg
-    }*/
+
     private fun rotateImage(img:Bitmap, degree:Int):Bitmap {
         //val img = BitmapFactory.decodeFile(path)
         val matrix = Matrix()
@@ -395,7 +280,7 @@ class EditProfileActivity : AppCompatActivity() {
         val nickname = nickname.text.toString()
         val email = email.text.toString()
         val location = location.text.toString()
-        var path = imagePath//imageFile?.absolutePath
+        var path = imagePath
         val user = User(name, "", nickname, email, location, path)
 
         Log.d ("MAD_LOG", "SEND-USER: $user")
