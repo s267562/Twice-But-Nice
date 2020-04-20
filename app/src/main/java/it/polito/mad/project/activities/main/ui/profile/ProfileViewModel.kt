4bde640d64d2c05ef201afbd0eaa7112ad196ef4
Klaus Cuko <klaus.cuko@gmail.com>
2020-04-20 20:19:41 +0200

    Insert ShowProfileFragment from ShowProfileActivity

--- app/src/main/java/it/polito/mad/project/activities/main/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/it/polito/mad/project/activities/main/ui/profile/ProfileViewModel.kt
@@ -3,11 +3,8 @@ package it.polito.mad.project.activities.main.ui.profile
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import it.polito.mad.project.models.User
 
 class ProfileViewModel : ViewModel() {
-
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is profile Fragment"
-    }
-    val text: LiveData<String> = _text
+    val user = MutableLiveData<User>()
 }
---
+++ app/src/main/java/it/polito/mad/project/activities/main/ui/profile/ShowProfileFragment.kt
@@ -0,0 +1,80 @@
+package it.polito.mad.project.activities.main.ui.profile
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.google.gson.Gson
+import it.polito.mad.project.R
+import it.polito.mad.project.enums.StoreFileKey
+import it.polito.mad.project.models.User
+import kotlinx.android.synthetic.main.activity_show_profile.*
+import java.io.File
+
+class ShowProfileFragment : Fragment() {
+
+    private lateinit var profileViewModel: ProfileViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        profileViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)
+
+
+        profileViewModel.user.observe(this.viewLifecycleOwner, Observer{
+            if (it.name.isNotEmpty())
+                full_name.text = it.name
+            if (it.nickname.isNotEmpty())
+                nickname.text = it.nickname
+            if (it.email.isNotEmpty())
+                email.text = it.email
+            if (it.location.isNotEmpty())
+                location.text = it.location
+            if (it.photoProfilePath != null && it.photoProfilePath.isNotEmpty()) {
+                if (File(it.photoProfilePath).isFile){
+                    val image: Bitmap = BitmapFactory.decodeFile(it.photoProfilePath)
+                    if (image != null) user_photo.setImageBitmap(image)
+                }
+            }
+        })
+        loadUserFromStoreFile()
+        return inflater.inflate(R.layout.activity_show_profile, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.option_menu, menu)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+        saveUserToStoreFile()
+    }
+
+    private fun loadUserFromStoreFile() {
+        // Load store file of our app from shared preferences
+        val sharedPreferences = this.activity?.getSharedPreferences(getString(R.string.app_store_file_name), Context.MODE_PRIVATE)
+
+        // Load from the store file the user object. For the first time we load empty string.
+        val userJson: String? = sharedPreferences?.getString(StoreFileKey.USER, "")
+
+        if (userJson != null && userJson.isNotEmpty()) {
+            // Assign the stored user to our view model if it is not empty
+            profileViewModel.user.value = Gson().fromJson(userJson, User::class.java)
+        }
+    }
+
+    private fun saveUserToStoreFile() {
+        val sharedPref = this.activity?.getSharedPreferences(getString(R.string.app_store_file_name), Context.MODE_PRIVATE)
+        val prefsEditor = sharedPref?.edit()
+        prefsEditor?.putString(StoreFileKey.USER, Gson().toJson(profileViewModel));
+        prefsEditor?.commit();
+    }
+}
