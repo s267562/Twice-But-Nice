67927a9d2375b64bfc122a1dbd9d80d084608e52
ivanaorefice <s266782@studenti.polito.it>
2020-04-23 12:28:43 +0200

    Adding ItemEditFragment

---
+++ app/src/main/java/it/polito/mad/project/activities/main/ui/advertisements/ItemEditFragment.kt
@@ -0,0 +1,7 @@
+package it.polito.mad.project.activities.main.ui.advertisements
+
+import androidx.fragment.app.Fragment
+
+class ItemEditFragment : Fragment() {
+
+}
---
+++ app/src/main/java/it/polito/mad/project/activities/main/ui/advertisements/ItemEditViewModel.kt
@@ -0,0 +1,7 @@
+package it.polito.mad.project.activities.main.ui.advertisements
+
+import androidx.lifecycle.ViewModel
+
+class ItemEditViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
