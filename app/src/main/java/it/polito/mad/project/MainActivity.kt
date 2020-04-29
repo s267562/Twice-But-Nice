545abc5c7db5e7c11035be8d36c56367c0b73c05
Victor Sinapi <s265925@studenti.polito.it>
2020-04-30 01:27:47 +0200

    Layout Drawer Header

--- app/src/main/java/it/polito/mad/project/MainActivity.kt
+++ app/src/main/java/it/polito/mad/project/MainActivity.kt
@@ -7,15 +7,24 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import it.polito.mad.project.R
+import it.polito.mad.project.enums.StoreFileKey
+import it.polito.mad.project.fragments.profile.ProfileViewModel
+import it.polito.mad.project.fragments.common.StoreFileFragment
+import it.polito.mad.project.models.User
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
+import kotlinx.android.synthetic.main.nav_header_main.*
+
 
 // POINT 2: Implement the MainActivity, controller of fragments and Navigation
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var profileViewModel: ProfileViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -46,5 +55,7 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
     }
+
 }
