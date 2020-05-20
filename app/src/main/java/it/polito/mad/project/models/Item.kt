7131e47ccc690cdb15871042a304ae806dda27d4
ivanaorefice <s266782@studenti.polito.it>
2020-05-20 11:00:32 +0200

    Point7: update item edit so that we can set the status of the item

--- app/src/main/java/it/polito/mad/project/models/Item.kt
+++ app/src/main/java/it/polito/mad/project/models/Item.kt
@@ -6,6 +6,7 @@ data class Item (var id: String?) : Serializable {
     var title: String = ""
     var category: String = ""
     var subcategory : String = " "
+    var status : String = ""
     var categoryPos: Int = -1
     var price: String = ""
     var description: String = ""
@@ -14,11 +15,12 @@ data class Item (var id: String?) : Serializable {
     var imagePath: String = ""
     var user: String=""
 
-    constructor(id: String, title: String, category: String, subcategory: String, price: Double,
+    constructor(id: String, title: String, category: String, subcategory: String, status: String, price: Double,
                 description: String, expiryDate: String, location: String, imagePath: String? = "") : this(id){
         this.title = title
         this.category = category
         this.subcategory = subcategory
+        this.status = status
         this.price = price.toString()
         this.description = description
         this.expiryDate = expiryDate
