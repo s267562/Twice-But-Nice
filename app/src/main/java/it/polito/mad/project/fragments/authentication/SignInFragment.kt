85eb97b3977e7e7b2dce1a53e55136d7a68d27d1
Simone <simone.sinagra96@gmail.com>
2020-05-24 03:02:57 +0200

    Revert "login GUI"
    This reverts commit 1b8eff699c38662ccce30350e6f1d1740db91988.

--- app/src/main/java/it/polito/mad/project/fragments/authentication/SignInFragment.kt
+++ app/src/main/java/it/polito/mad/project/fragments/authentication/SignInFragment.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -74,7 +73,7 @@ class SignInFragment : Fragment() {
             authViewModel.loginWithEmailPassword(log_email.text.toString(), log_password.text.toString())
         }
 
-        signup_button.setOnClickListener {
+        logToReg.setOnClickListener {
             findNavController().navigate(R.id.action_navHome_to_signUpFragment)
         }
     }
--- app/src/main/java/it/polito/mad/project/fragments/authentication/SignUpFragment.kt
+++ app/src/main/java/it/polito/mad/project/fragments/authentication/SignUpFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
@@ -13,7 +12,6 @@ import androidx.navigation.fragment.findNavController
 import it.polito.mad.project.R
 import it.polito.mad.project.models.User
 import it.polito.mad.project.viewmodels.AuthViewModel
-import kotlinx.android.synthetic.main.fragment_login.*
 import kotlinx.android.synthetic.main.fragment_signup.*
 
 class SignUpFragment: Fragment() {
@@ -22,7 +20,6 @@ class SignUpFragment: Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         authViewModel = ViewModelProvider(requireActivity()).get(AuthViewModel::class.java)
-
     }
 
     override fun onStart() {
