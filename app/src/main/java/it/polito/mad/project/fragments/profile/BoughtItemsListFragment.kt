987c36150c6aa486ebd18596a0f17e8c2c3e245b
ivanaorefice <s266782@studenti.polito.it>
2020-05-28 15:55:24 +0200

    Creazione nuovi fragment (Bought Items e Items of Interest)

--- app/src/main/java/it/polito/mad/project/fragments/profile/BoughtItemsListFragment.kt
+++ app/src/main/java/it/polito/mad/project/fragments/profile/BoughtItemsListFragment.kt
@@ -4,13 +4,29 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import it.polito.mad.project.R
+import it.polito.mad.project.viewmodels.ItemViewModel
 
 class BoughtItemsListFragment : Fragment() {
+
+    private lateinit var itemViewModel : ItemViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        itemViewModel = ViewModelProvider(activity?:this).get(ItemViewModel::class.java)
+    }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_bought_items_list, container, false)
     }
+
+    override fun onStart() {
+        super.onStart()
+        (activity as AppCompatActivity?)?.supportActionBar?.show()
+    }
+
 
 }
