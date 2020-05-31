6bfa301f8dc693f328796c4baa2bb312cb5bff82
Klaus Cuko <klaus.cuko@gmail.com>
2020-05-31 23:57:36 +0200

    fix(src): some minor code review

--- app/src/main/java/it/polito/mad/project/fragments/advertisements/DialogViewFragment.kt
+++ app/src/main/java/it/polito/mad/project/fragments/advertisements/DialogViewFragment.kt
@@ -2,14 +2,15 @@ package it.polito.mad.project.fragments.advertisements
 
 import android.app.Application
 import android.app.Dialog
-import android.content.DialogInterface
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.DialogFragment
 import it.polito.mad.project.R
 import it.polito.mad.project.fragments.advertisements.StringGlobal.Companion.globalFilter
+import java.util.*
 
 class DialogViewFragment : DialogFragment() {
 
@@ -17,7 +18,7 @@ class DialogViewFragment : DialogFragment() {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
             // Get the layout inflater
-            val inflater = requireActivity().layoutInflater;
+            val inflater: LayoutInflater = requireActivity().layoutInflater
             val viewToInflate: View = inflater.inflate(R.layout.modal_filter, null)
 
             val spinner = viewToInflate.findViewById<Spinner>(R.id.filter_spinner)
@@ -34,21 +35,21 @@ class DialogViewFragment : DialogFragment() {
             // Pass null as the parent view because its going in the dialog layout
             builder.setView(viewToInflate)
                 // Add action buttons
-                .setPositiveButton("Set",
-                    DialogInterface.OnClickListener { dialog, id ->
-                        // set up the spinner and pass the filter string
-                        globalFilter = spinner.selectedItem.toString().toLowerCase()
-                        Toast.makeText(requireContext(), "You set the filter to: " + globalFilter, Toast.LENGTH_SHORT).show()
-                        dialog.cancel()
-                    })
-                .setNegativeButton("Cancel",
-                    DialogInterface.OnClickListener { dialog, id ->
-                        dialog.cancel()
-                    })
+                .setPositiveButton("Set"
+                ) { dialog, _ ->
+                    // set up the spinner and pass the filter string
+                    globalFilter = spinner.selectedItem.toString().toLowerCase(Locale.ROOT)
+                    Toast.makeText(requireContext(), "You set the filter to: $globalFilter", Toast.LENGTH_SHORT).show()
+                    dialog.cancel()
+                }
+                .setNegativeButton("Cancel"
+                ) { dialog, _ ->
+                    dialog.cancel()
+                }
 
-            if(spinner.selectedItem.toString().toLowerCase() == "price"){
+            if(spinner.selectedItem.toString().toLowerCase(Locale.ROOT) == "price"){
                 Toast.makeText(requireContext(),
-                    spinner.selectedItem.toString().toLowerCase(),
+                    spinner.selectedItem.toString().toLowerCase(Locale.ROOT),
                     Toast.LENGTH_SHORT).show()
                 val linearLayoutPrice = viewToInflate.findViewById<LinearLayout>(R.id.price_range)
                 linearLayoutPrice.visibility = View.VISIBLE
