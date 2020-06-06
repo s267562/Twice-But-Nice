0999f02564022b5a34c95117eaea114ce8ced1e3
Victor Sinapi <s265925@studenti.polito.it>
2020-06-06 14:55:37 +0200

    Fix edit profile photo without save bug

--- app/src/main/java/it/polito/mad/project/fragments/profile/UserEditFragment.kt
+++ app/src/main/java/it/polito/mad/project/fragments/profile/UserEditFragment.kt
@@ -161,7 +161,7 @@ class UserEditFragment : Fragment() {
             val rotateBitmap = rotateImage(image!!)
             image = rotateBitmap
             user_photo.setImageBitmap(image)
-            userViewModel.user.image.value = image
+            //userViewModel.user.image.value = image
         }
 
         location.setOnClickListener {
@@ -287,7 +287,7 @@ class UserEditFragment : Fragment() {
         } else {
             Toast.makeText(mContext, "Something wrong", Toast.LENGTH_SHORT).show()
         }
-        userViewModel.user.image.value = (user_photo.drawable as BitmapDrawable).bitmap
+        //userViewModel.user.image.value = (user_photo.drawable as BitmapDrawable).bitmap
     }
 
     @SuppressLint("SimpleDateFormat")
